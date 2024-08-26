0171c90760219f90386d80633752deea28e0bc56
Sahbaz4321 <168401466+Sahbaz4321@users.noreply.github.com>
2024-08-27 01:26:04 +0530

    Add files via upload

---
+++ Opearator.kt
@@ -0,0 +1,40 @@
+fun main(){
+
+// logical operator
+    var x=4;
+    var y=6;
+//    if(x>2 && y<10){
+//        println("you entered")
+//    }
+//    else{
+//        println("not entered")
+//    }
+//
+//    if(x<2 || y<10){
+//        println("you entered")
+//    }
+//    else{
+//        println("not entered")
+//    }
+//
+//    println(x!=y)
+
+// bitwise operator
+
+    println(x and y);
+    println(x or y);
+    println(x xor y)
+
+
+    println(x.shl(1))
+    println(x.shr(1))
+
+
+
+
+
+
+
+
+
+}
