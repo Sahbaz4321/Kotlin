fbeef317feef249a9ed5b0ebdc75f36921d72f7b
Sahbaz4321 <168401466+Sahbaz4321@users.noreply.github.com>
2024-08-27 01:25:21 +0530

    Add files via upload

---
+++ forLoop.kt
@@ -0,0 +1,34 @@
+fun main(){
+
+//    var arr=arrayOf("cars","bmw","mercedees")
+//    for(i in arr.indices){
+//        println(i)
+//    }
+//    for(i in 1..10){
+//        println(i)
+//    }
+//    for(i in  10 downTo 1 step 2){
+//        println(i)
+//    }
+//    for(i in 1..15){
+//        if(i==10){
+//            break;
+//        }
+//        println(i)
+//    }
+
+//    for(i in 1..15){
+//        if(i==10){
+//            continue
+//        }
+//        println(i)
+//    }
+
+//    for(i in 6 until 10){
+//        println(i)
+//    }
+
+
+
+
+}
